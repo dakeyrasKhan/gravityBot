41555fee9574ce5cf40e3378fd3ce15dad9db545
Ulysse Beaugnon <ulysse.beaugnon@free.fr>
2013-01-28 11:55:18 +0100

    Ozcollision is now linked into the project

--- code/Scene.h
+++ code/Scene.h
@@ -3,19 +3,33 @@
 #include <queue>
 #include <algorithm>
 #include <array>
+#include <ozcollide/ozcollide.h>
+#include <ozcollide/aabbtree_poly.h>
 #include "octree.hpp"
 #include "path.hpp"
 #include "Vector.hpp"
+
 
 class Scene
 {
 public:
 	Scene(const char* sceneFile);
-	std::vector<Point> points;
-	std::vector<std::array<int, 3>> triangles;
+	~Scene();
+	const std::vector<Point> Points() const { return points; };
+	const std::vector<std::array<int, 3>> Triangles() const { return triangles; };
+
 	std::array<double, DIM_CONF> size;
 	double maxSize(){ return *std::max_element(size.begin(),size.end()); }
 	bool collision(Position);
 	bool validMove(Position, Position);
+
+private:
+	void ReadObjFile(const char* fileName);
+	void BuildCollisionTree();
+
+	std::vector<Point> points;
+	std::vector<std::array<int, 3>> triangles;
+
+	ozcollide::AABBTreePoly* collisionTree;
 };
 
